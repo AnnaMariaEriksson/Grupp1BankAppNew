0feeef3f6f06c36d215e3cca934298cb3c191209
Andre Röygaard <andre.roygaard@gmail.com>
2019-10-24 16:48:22 +0200

    Lade till customer klassen och skapade en tillfällig Account objekt som ska tas bort sen.

---
+++ Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp1BankApp
+{
+
+    class Account { }
+
+   abstract class Customer
+    {
+        public List<Account> CustomerAccounts = new List<Account>();
+        private string name;
+        private string ssn;
+        
+        public Customer(string Name, string Ssn)
+        {
+            name = Name;
+            ssn = Ssn;
+         }
+
+ 
+   
+
+
+    }
+}
