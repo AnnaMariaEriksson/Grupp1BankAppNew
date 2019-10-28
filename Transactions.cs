342545cdaec6771b0dd19e4e72917d1d50c28a08
Saimir avdija <katundari_@hotmail.com>
2019-10-28 10:38:13 +0100

    Created the class transactions

---
+++ Transactions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp1BankApp
+{
+    
+    class Transactions
+    {
+        private string accountID;
+        private DateTime date;
+        private Enum transactionsType;
+        private double amount;
+        private double balance;
+        public Transactions(string AccountID, DateTime Date, Enum TransactionsType, double Amount, double Balance)
+        {
+            accountID = AccountID;
+            date = Date;
+            transactionsType = TransactionsType;
+            amount = Amount;
+            balance = Balance;
+        }
+    }
+}
