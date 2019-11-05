e9b83aa83ba41ce65e619afc4e731ee335a14fe5
Saimir avdija <katundari_@hotmail.com>
2019-11-05 13:43:47 +0100

    redigerade ikonen

--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -22,6 +22,7 @@ namespace Grupp1BankApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
     public sealed partial class MainPage : Page
     {
 
